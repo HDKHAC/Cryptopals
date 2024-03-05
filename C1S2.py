8df421286540e0a299d8985ed7c5a264a7117066
HDKHAC <huyduong.k@gmail.com>
2024-03-05 02:22:44 -0800

    No need for import.

--- C1S2.py
+++ C1S2.py
@@ -1,17 +1,15 @@
-import binascii
-
 # Inputs
 buffer1_hex_string = "1c0111001f010100061a024b53535009181c"
 buffer2_hex_string = "686974207468652062756c6c277320657965"
 
 def xor_buffers(buffer1_hex_string, buffer2_hex_string):
     # hex string > bytes
-    buffer1_rawb = binascii.unhexlify(buffer1_hex_string)
-    buffer2_rawb = binascii.unhexlify(buffer2_hex_string)
+    buffer1_rawb = bytes.fromhex(buffer1_hex_string)
+    buffer2_rawb = bytes.fromhex(buffer2_hex_string)
     # XOR the two buffers by iterate through tuples
     xor_rawb = bytes(x ^ y for x, y in zip(buffer1_rawb, buffer2_rawb))
     # bytes > hex string
-    xor_hex_string = binascii.hexlify(xor_rawb).decode('utf-8')
+    xor_hex_string = xor_rawb.hex()
     return xor_hex_string
 
 xor_hex_string = xor_buffers(buffer1_hex_string, buffer2_hex_string)
